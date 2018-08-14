e8b8816401405f0dfca9290280c6769d69c3d2f3
André Machado <easmveiga@gmail.com>
2018-08-14 15:50:33 -0300

    06-08-2018 Número primos

--- numeroPrimo.java
+++ numeroPrimo.java
@@ -1,21 +1,11 @@
 import java.util.Scanner;
-/**
- * Verifica se um número é primo ou não.
- * 
- * @author André Machado
- * @version 06/08/2018
- */
 public class numerosPrimos
 {
     public static void main(String[] args){
-        
         Scanner entrada = new Scanner(System.in);
         int numero = entrada.nextInt(),i=2;
-        
         String resposta;
-        
         while(numero%i!=0){i++;}
-        
         resposta = numero == i ? " é primo!" : " não é primo!";
         System.out.println("O valor " + numero + resposta);
     }
